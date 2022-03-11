55775ac9ec305efb1c1400510d6fd7e5e4549ef1
Jayromulus <jerbear2@gmail.com>
2022-03-11 17:58:57 -0500

    basic dice rolls added

--- index.js
+++ index.js
@@ -10,13 +10,45 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', async (message) => {
-  // console.log(message);
   if (message.author.bot) return;
-  //if (message.channelId === process.env.CHANNEL_1 || message.channelId === process.env.CHANNEL_2) {
   if (message.content.indexOf(process.env.PREFIX) !== 0) return;
   const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  message.channel.send('response');
+  //message.channel.send(`testing answer to: ${command}`);
+  switch(command) {
+    case 'roll':
+      // ?roll 1d6: roll 1d6 non exploding | ?roll 1d6!: roll 1d6 exploding (on max roll another die of same size)
+      let rolls = [];
+      args.forEach(roll => {
+        let rollMod = roll.trim().split('+');
+        let bonus = parseInt(rollMod[1]);
+        let numBase = rollMod[0].split('d');
+	if (rollMod[0][rollMod[0].length-1] === '!'){
+          let currentRoll = 0;
+          while(currentRoll < numBase[0]){
+            let result;
+            result = rollDice(parseInt(numBase[1]));
+            rolls.push(result);
+            while (result === parseInt(numBase[1])){
+              result = rollDice(parseInt(numBase[1]));
+              rolls.push(result);
+            }
+            currentRoll++;
+        }
+        } else {
+          for(let n = 0; n < numBase[0]; n++){
+            rolls.push(rollDice(parseInt(numBase[1]), bonus));
+          }
+        }
+      })
+      console.log(rolls);
+      message.channel.send(`total: ${rolls.reduce((a,b) => a + b)}`);
+  }
 });
+
+function rollDice(base, bonus) {
+  let rand = Math.floor(Math.random() * base + 1) + (bonus ? bonus : 0);
+  return rand;
+}
 
 client.login(process.env.TOKEN);
