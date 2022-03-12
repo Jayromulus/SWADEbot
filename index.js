1bc333bef2fbfc1f3149e568bbe93c4fa4562344
Jayromulus <jerbear2@gmail.com>
2022-03-11 21:02:14 -0500

    show multiple rolls seperate

--- index.js
+++ index.js
@@ -15,34 +15,43 @@ client.on('messageCreate', async (message) => {
   const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   //message.channel.send(`testing answer to: ${command}`);
-  switch(command) {
+  switch (command) {
     case 'roll':
-      // ?roll 1d6: roll 1d6 non exploding | ?roll 1d6!: roll 1d6 exploding (on max roll another die of same size)
-      let rolls = [];
-      args.forEach(roll => {
-        let rollMod = roll.trim().split('+');
-        let bonus = parseInt(rollMod[1]);
-        let numBase = rollMod[0].split('d');
-	if (rollMod[0][rollMod[0].length-1] === '!'){
-          let currentRoll = 0;
-          while(currentRoll < numBase[0]){
-            let result;
-            result = rollDice(parseInt(numBase[1]));
-            rolls.push(result);
-            while (result === parseInt(numBase[1])){
+      if (args[0] === 'help') {
+        message.channel.send('SWADEbot is a dice roller app that helps you roll multiple dice, be they exploding or nay!\n\nROLLING DICE\nIn order to roll dice, all you have to do it type the command `?roll` followed by the number and type of dice you would like to roll.\n\nEXAMPLE 1:\n`?roll 1d6`\ngives you a number result of rolling one six-sided die\n\nEXAMPLE 2:\n`?roll 1d8+2`\ngives you the combined result of an 8 sided die with the bonus of 2\n\nEXAMPLE 3:\n`?roll 1d6 1d4`\nrolls 1d6 and 1d4 giving you both results as well as the total');
+      } else {
+        // ?roll 1d6: roll 1d6 non exploding | ?roll 1d6!: roll 1d6 exploding (on max roll another die of same size)
+        let rolls = [];
+        args.forEach(roll => {
+          let rollMod = roll.trim().split('+');
+          let bonus = parseInt(rollMod[1]);
+          let numBase = rollMod[0].split('d');
+          if (rollMod[0][rollMod[0].length - 1] === '!') {
+            let currentRoll = 0;
+            let currentDice = [];
+            while (currentRoll < numBase[0]) {
+              let result;
               result = rollDice(parseInt(numBase[1]));
-              rolls.push(result);
+              currentDice.push(result);
+              while (result === parseInt(numBase[1])) {
+                result = rollDice(parseInt(numBase[1]));
+                currentDice.push(result);
+              }
+              currentRoll++;
             }
-            currentRoll++;
-        }
-        } else {
-          for(let n = 0; n < numBase[0]; n++){
-            rolls.push(rollDice(parseInt(numBase[1]), bonus));
+            rolls.push(currentDice)
+          } else {
+            let currentDice = [];
+            for (let n = 0; n < numBase[0]; n++) {
+              currentDice.push(rollDice(parseInt(numBase[1]), bonus));
+            }
+            rolls.push(currentDice);
           }
-        }
-      })
-      console.log(rolls);
-      message.channel.send(`total: ${rolls.reduce((a,b) => a + b)}`);
+        })
+        console.log(rolls);
+// fix the issue where multiple dice rolls are not going to display in the same line, (maybe make an object each time you roll a section and reference the object according to the argument index?
+        message.channel.send(`total: ${rolls.reduce((a, b) => parseInt(b) +  a.reduce((c,d) => parseInt(c) + parseInt(d)))}${rolls.map((r, i) => `\n${args[i]}: ${r}`).join('')}`);
+      }
   }
 });
 
