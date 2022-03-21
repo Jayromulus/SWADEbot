89fdc1f4822a288a8f27392ec482d9f7d260c267
Jayromulus <jerbear2@gmail.com>
2022-03-20 22:06:30 -0400

    began documentation of index

--- index.js
+++ index.js
@@ -4,51 +4,77 @@ const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"], partials: ['MESSAGE', 'REACTION', 'CHANNEL'] });
 const e = require('express');
 const weapons = require('./weapons.json');
-const w_embed_length = 24;
+const w_embed_length = 18;
 
+// login to the client
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+  // register the activity tag of the bot
   client.user.setActivity('Running some test, hopefully.');
 })
 
+// listen for a message to be sent in the channel, kind of a shotgun approach but it works well
 client.on('messageCreate', async (message) => {
+  // if the bot sent it then ignore the message
   if (message.author.bot) return;
+  // if the message does not begin with the specified command prefix then ignore it
   if (message.content.indexOf(process.env.PREFIX) !== 0) return;
+  // make a list of the original arguments sent to the bot, since the argument list is modified slightly throughout the process
   const origin_args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  // the argument list that is used by the bot and modified into the final product used to roll the dice or otherwise search
   const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+  // this will grab the command which is the first arg passed after the prefix
   const command = args.shift().toLowerCase();
+  // switch through potential commands
   switch (command) {
+    // ?help
     case 'help':
+      // send a message detailing how to use the bot to the user
       message.channel.send('***SWADEbot is a dice roller app that helps you roll multiple dice, be they exploding or nay!***\n*NOTE: Anything inside of < > means it can be replaced with anything matching that pattern, e.g. `<#d#>` can be replaced with `1d6`. Anything with a `?` in front of it is an optional input*\n\n**ROLLING DICE**\nIn order to roll dice, all you have to do it type the command `?roll` followed by the number and type of dice you would like to roll.\n\n**EXAMPLE 1:**\n`?roll 1d6`\ngives you a number result of rolling one six-sided die\n*notation:* `?roll <#d#>`\n\n**EXAMPLE 2:**\n`?roll 1d8+2`\ngives you the combined result of an 8 sided die with the bonus of 2\n*notation:* `?roll <#d#+#>`\n\n**EXAMPLE 3:**\n`?roll 1d6 1d4`\nrolls 1d6 and 1d4 giving you both results as well as the total\n*notation:* `?roll <#d#> ?<#d#> ... ?<#d#+#>`\n\n**EXAMPLE 4:**\n`?roll shortsword 1d8!`\nrolls damage for a shortsword (1d6 + str) using 1d8 (exploding) as the strength score\n*notation:* `?roll <weapon> <strength> ?<extra dice>`\n\n**WEAPON LIST**\nto see a list of weapons, simply use the command\n`?weapons`\n\n**HELP**\nto see a list of commands, use the command\n`?help`\n*yes SWADEbot can see the irony in needing to use a command to see the list of commands*');
       break;
+    // ?weapons
     case 'weapons':
+      // grab the serach term from the argument list
       let search = args[0];
-      // console.log(search);
+      // if the user has a specific search termn
       if (search) {
-        let current = weapons[search];
-        let currentFields = Object.keys(current).map(c => {
-          if (current[c] !== '' && current[c] !== 0 && c !== 'source') {
-            return {
-              name: c.split('_').join(' '),
-              value: current[c].toString(),
-              inline: true
+        // the current weapon will look in the json for the item entered by the user
+        let current = weapons[search.toLowerCase()];
+        if(current) {
+          // map over the keys of the searched weapon and create embed field objects for each of them
+          let currentFields = Object.keys(current).map(c => {
+            // check to make sure that each current value is not either empty or the hnumber 0, as well as not showing the source (this will be in the footer)
+            if (current[c] !== '' && current[c] !== 0 && c !== 'source') {
+              return {
+                name: c.split('_').join(' '),
+                value: current[c].toString(),
+                inline: true
+              }
             }
-          }
-        });
-        //console.log('current:', currentFields)
-        let currentEmbed = new Discord.MessageEmbed()
-          .setColor('#6C7079')
-          .setTitle(search.split('_').join(' '))
-          .setFooter({ text: weapons[search].source })
-          .addFields(currentFields.filter(e => e !== undefined));
-        message.channel.send({ embeds: [currentEmbed] });
+          });
+          // make a new discord embed
+          let currentEmbed = new Discord.MessageEmbed()
+            .setColor('#6C7079')
+            .setTitle(search.split('_').join(' '))
+            .setFooter({ text: weapons[search].source })
+            .addFields(currentFields.filter(e => e !== undefined));
+          message.channel.send({ embeds: [currentEmbed] });
+        // "catch" in case the item is not in the json
+        } else {
+          message.channel.send(`could not find weapon ${search}`);
+        }
+      // if there is no search term, this will send out the whole list of items
       } else {
+        // create the list of weapons from the list of keys in the weapons.json
         let weaponsList = [];
         weaponsList.push(...Object.keys(weapons).sort().filter(e => e.name !== ''));
-        const page_total = Math.ceil(weaponsList.length / 24);
+        // grab the total number of pages according to the number of items needed in the embed
+        const page_total = Math.ceil(weaponsList.length / w_embed_length);
+        // create the initial embed
         let currentEmbed = weaponsEmbed(0, page_total, 0);
 
         // not very performant right now, but it works well to paginate the data and view it all
+        // creating the initial forward and back buttons to be used in the embed actions
         const backId = 'back'
         const forwardId = 'forward'
         const backButton = new MessageButton({
@@ -65,35 +91,39 @@ client.on('messageCreate', async (message) => {
         })
 
         const canFitOnOnePage = weaponsList.length <= w_embed_length
+        // send the embed and add the button action row in case there are more weapons that can fit on one page
         const embedMessage = await message.channel.send({
           embeds: [currentEmbed],
           components: canFitOnOnePage
             ? []
             : [new MessageActionRow({ components: [forwardButton] })]
         })
-        // Exit if there is only one page of guilds (no need for all of this)
+
+        // if everything can fit on one page, just exit the function
         if (canFitOnOnePage) return
 
-        // Collect button interactions (when a user clicks a button),
-        // but only when the button as clicked by the original message author
+        // create an interaction collector to respond when someone clicks an action button
         const collector = embedMessage.createMessageComponentCollector({
           filter: ({ user }) => true
         })
 
+        // set the current page number to 0
         let currentIndex = 0
+        // when an interaction is made
         collector.on('collect', async interaction => {
-          // Increase/decrease index
+          // if the back button is pressed, subtract from the starting point, otherwise add to the starting point of the display
           interaction.customId === backId ? (currentIndex -= w_embed_length) : (currentIndex += w_embed_length)
-          // Respond to interaction by updating message with new embed
+          // update the embed according to the interaction made
           await interaction.update({
+            // create a new embed using the new starting position
             embeds: [weaponsEmbed(currentIndex, page_total, currentIndex)],
             components: [
               new MessageActionRow({
                 components: [
-                  // back button if it isn't the start
+                  // if we are on the first page do not show the back button
                   ...(currentIndex ? [backButton] : []),
-                  // forward button if it isn't the end
-                  ...(currentIndex + 24 < weaponsList.length ? [forwardButton] : [])
+                  // if we are on the last page do not show the forward button
+                  ...(currentIndex + w_embed_length < weaponsList.length ? [forwardButton] : [])
                 ]
               })
             ]
@@ -101,30 +131,45 @@ client.on('messageCreate', async (message) => {
         })
       }
       break;
+    // ?roll
     case 'roll':
+      // create the empty list of all rolls
       let rolls = [];
+      // if the first argument of rolls is a weapon in the json
       if (args[0] in weapons) {
-        let tempArgs = weapons[args[0]].damage.split(',');
-        args.shift();
+        // grab the damage for the weapon from the json while removing it from the args, and split it up if there is a strength section in it
+        let tempArgs = weapons[args.shift()].damage.split(',');
+        // remove the strength from the args and store it
         let str = args.shift();
+        // put the weapon damage back at the beginning of the array
         args.unshift(...tempArgs);
+        // if there is a strength die required for the weapon damage
         if (str) {
+          // temp is going to be the args array without the str string in it
           let temp = [...args.filter(e => e != 'str')];
+          // empty the args array
           while (args[0]) { args.shift() };
+          // then refill it with the temp array, now that it should only have dice in it
           while (temp[0]) { args.push(temp.shift()) };
-          let weaponDie = args[args.length - 1];
-          if (parseInt(weapons[origin_args[1]].min_str.split(/\D/)[1]) > parseInt(str.split(/\D/)[1])) {
-            args.pop();
+          // grabs the weapon die from the end of the array, now that the array should only contain the weapon die, this is to make the next few lines more legible
+          let weaponDie = args[0];
+          // if the weapon includes a strength die and the strength die provided by the user is below the minimum strength
+          if (weapons[origin_args[1]].damage.includes('str') && parseInt(weapons[origin_args[1]].min_str.split(/\D/)[1]) > parseInt(str.split(/\D/)[1])) {
+            // remove the original weapon damage since we will need to downscale the die in accordance to the user's strength (CITATION NEEDED)
+            let originDamage = args.pop();
+            // debugging comparison
             console.log(weapons[origin_args[1]].min_str.split(/\D/)[1] + '|' + str.split(/\D/)[1]);
-            args.unshift(weaponDie.split(/\D/)[0] + 'd' + str.split(/\D/)[1]);
+            // add the reduced die to the front of the args array, with it exploding if the original damage was exploding
+            args.unshift(weaponDie.split(/\D/)[0] + 'd' + str.split(/\D/)[1] + (originDamage.includes('!') ? '!' : ''));
           }
+          // add the strength back into the args array after the first index, this is so the position is correct when displaying results
           args.splice(1, 0, str);
         }
       }
       let exp = /\d+d\d+!?/;
       let malformed = args.some(e => !exp.test(e));
       if (malformed) {
-        message.channel.send('Missing Strength Parameter');
+        message.channel.send('Invalid command syntax');
       } else {
         args.forEach(roll => {
           let rollMod = roll.trim().split('+');
@@ -154,7 +199,6 @@ client.on('messageCreate', async (message) => {
         let weaponTest = /\d+d\d+!?/;
         let weaponRoll = !weaponTest.test(origin_args[0]);
         let isWeapon = !weaponTest.test(origin_args[1]);
-        // console.log(args[0], weaponRoll)
         args.map((a, i) => {
           let display = `\n${weaponRoll && i === 0 ? origin_args[1] : isWeapon && weapons[origin_args[1]].damage.includes('str') && i === 1 ? 'strength' : a}: `;
           for (let n = 0; n < a.split(/\D/)[0]; n++) {
