1fdeff25a0b4ee8cbbcad1186f21375575e0a199
Jayromulus <jerbear2@gmail.com>
2022-03-25 13:54:17 -0400

    what the bonus doin?

--- index.js
+++ index.js
@@ -251,6 +251,7 @@ client.on('messageCreate', async (message) => {
 
 // function to roll dice using the base size as the limit then adding the bonus if any is provided
 function rollDice(base, bonus) {
+  console.log(bonus)
   let rand = Math.floor(Math.random() * base + 1) + (bonus ? bonus : 0);
   return rand;
 }
