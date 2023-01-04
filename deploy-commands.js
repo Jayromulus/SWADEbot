4b2b810b93fbb77cbd612f9a978c2638b397abbe
Jerome Martin Flaherty <jerbear2@gmail.com>
2023-01-04 01:37:34 -0500

    testing something to reset commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,25 +13,26 @@ for (const file of commandFiles) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.PROD_TOKEN);
 
 // and deploy your commands!
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-    const deleted = await rest.put(Routes.applicationCommands(process.env.CLIENT), { body: [] })
-    .then(() => console.log('Successfully deleted all application commands.'))
+    await rest.put(Routes.applicationGuildCommands(process.env.PROD_CLIENT, process.env.GUILD), { body: [] })
+    .then(() => console.log('Successfully deleted all guild commands.'))
     .catch(console.error);
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD),
-      // comment above and uncomment below to make comands global
-    	// Routes.applicationCommands(process.env.CLIENT),
-			{ body: commands },
-		);
+		// const data = await rest.put(
+		// 	Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD),
+    //   // comment above and uncomment below to make comands global
+    // 	// Routes.applicationCommands(process.env.CLIENT),
+		// 	{ body: commands },
+		// );
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    // ${data.length}
+		console.log(`Successfully reloaded  application (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
